8cf06544e23e569a7d493ab2c7b0dd8bc111c894
AldG99 <a.garciaglz17@gmail.com>
2024-07-13 20:57:41 -0600

    Buscar un usuario en Firebase Firestore

--- src/components/addUser/addUser.js
+++ src/components/addUser/addUser.js
@@ -1,20 +1,44 @@
+import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import "../../styles/addUser.scss";
+import { db } from "../../lib/firebaseConfig";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const AddUser = () => {
+  const [user, setUser] = useState(null)
+
+  const handleSearch = async e => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const username = formData.get("username")
+
+    try {
+      const userRef = collection(db, "users");
+
+      const q = query(userRef, where("username", "==", username));
+      const querySnapShop = await getDocs(q);
+
+      if(!querySnapShop.empty) {
+        setUser(querySnapShop.docs[0].data());
+      }
+
+    } catch(err) {
+      console.log(err);
+    }
+  }
   return (
     <div className="addUser">
-      <form>
+      <form onSubmit={handleSearch}>
         <input type="text" placeholder="Username" name="username" />
         <button>Búsqueda</button>
       </form>
-      <div className="user">
+      {user && <div className="user">
         <div className="detail">
           <FaUserCircle className="avatar" />
-          <span>Will Smith</span>
+          <span>{user.username}</span>
         </div>
         <button>Añadir Usuario</button>
-      </div>
+      </div>}
     </div>
   )
 }
