35836e8c8fba944815f9a5d4619cb68e394f69a2
AldG99 <a.garciaglz17@gmail.com>
2024-07-15 19:38:13 -0600

    Añadir un Nuevo Mensaje de Chat

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,38 +1,81 @@
 import React, { useEffect, useState, useRef } from "react";
-import { FaUserCircle, FaPhone, FaVideo, FaInfoCircle, FaSmile, FaImage, FaCamera, FaMicrophone } from "react-icons/fa"; // Importa los iconos necesarios
+import { FaUserCircle, FaPhone, FaVideo, FaInfoCircle, FaSmile, FaImage, FaCamera, FaMicrophone } from "react-icons/fa";
 import EmojiPicker from "emoji-picker-react";
 import "../../styles/chat.scss";
-import { doc, onSnapshot } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../lib/firebaseConfig";
 import { useChatStore } from "../../lib/chatStore";
+import { useUserStore } from "../../lib/userStore";
 
 const Chat = () => {
   const [chat, setChat] = useState();
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
 
-  const { chatId } = useChatStore();
+  const { currentUser } = useUserStore();
+  const { chatId, user } = useChatStore();
 
   const endRef = useRef(null);
 
   useEffect(() => {
-    endRef.current?.scrollIntoView({behavior: "smooth" })
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
   }, []);
 
   useEffect(() => {
     const unSub = onSnapshot(doc(db, "chats", chatId), (res) => {
-      setChat(res.data())
-    })
+      setChat(res.data());
+    });
 
     return () => {
       unSub();
-    }
+    };
   }, [chatId]);
 
-  const handleEmoji = e => {
+  const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
-    setOpen(false)
-  }
+    setOpen(false);
+  };
+
+  const handleSend = async () => {
+    if (text === "") return;
+
+    try {
+      await updateDoc(doc(db, "chats", chatId), {
+        messages: arrayUnion({
+          senderId: currentUser.id,
+          text,
+          createAt: new Date(),
+        }),
+      });
+
+      const userIDs = [currentUser.id, user.id];
+
+      userIDs.forEach(async (id) => {
+        const userChatsRef = doc(db, "userchats", id);
+        const userChatsSnapshot = await getDoc(userChatsRef);
+
+        if (userChatsSnapshot.exists()) {
+          const userChatsData = userChatsSnapshot.data();
+
+          if (userChatsData.chats) {
+            const chatIndex = userChatsData.chats.findIndex((c) => c.chatId === chatId);
+
+            if (chatIndex !== -1) {
+              userChatsData.chats[chatIndex].lastMessage = text;
+              userChatsData.chats[chatIndex].isSeen = id === currentUser.id;
+              userChatsData.chats[chatIndex].updateAt = Date.now();
+
+              await updateDoc(userChatsRef, {
+                chats: userChatsData.chats,
+              });
+            }
+          }
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="chat">
@@ -51,34 +94,35 @@ const Chat = () => {
         </div>
       </div>
       <div className="center">
-        {chat?.messages?.map(message => (
-            <div className="message own" key={message?.createAt}>
-             <div className="texts">
-                {message.img && <img className='imagen' src={message.img} alt="" />}
-                <p>{message.text}</p>
-            {/* <span>{message}</span> */}
+        {chat?.messages?.map((message) => (
+          <div className="message own" key={message?.createAt}>
+            <div className="texts">
+              {message.img && <img className="imagen" src={message.img} alt="" />}
+              <p>{message.text}</p>
             </div>
           </div>
         ))}
         <div ref={endRef}></div>
       </div>
-      <div className="buttom">
+      <div className="bottom">
         <div className="icons">
           <FaImage className="icon" />
           <FaCamera className="icon" />
           <FaMicrophone className="icon" />
         </div>
-        <input type="text" placeholder="Escribe un mensaje..." value={text} onChange={e => setText(e.target.value)} />
+        <input type="text" placeholder="Escribe un mensaje..." value={text} onChange={(e) => setText(e.target.value)} />
         <div className="emoji">
           <FaSmile className="emojiIcon" onClick={() => setOpen((prev) => !prev)} />
           <div className="picker">
             <EmojiPicker open={open} onEmojiClick={handleEmoji} />
           </div>
         </div>
-        <button className="sendButton">Enviar</button>
+        <button className="sendButton" onClick={handleSend}>
+          Enviar
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Chat;
--- src/components/list/chatList/chatList.js
+++ src/components/list/chatList/chatList.js
@@ -3,7 +3,7 @@ import { FaSearch, FaPlus, FaMinus } from "react-icons/fa";
 import "../../../styles/chatList.scss";
 import AddUser from "../../addUser/addUser";
 import { useUserStore } from "../../../lib/userStore";
-import { doc, onSnapshot, getDoc } from "firebase/firestore";
+import { doc, onSnapshot, getDoc, updateDoc } from "firebase/firestore"; // Asegúrate de que todas las funciones provienen del mismo SDK
 import { db } from "../../../lib/firebaseConfig";
 import { useChatStore } from "../../../lib/chatStore";
 
@@ -18,33 +18,51 @@ const ChatList = () => {
     const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async (res) => {
       if (res.exists()) {
         const items = res.data().chats || [];
-  
+
         const promises = items.map(async(item) => {
           const userDocRef = doc(db, "users", item.receiverId);
           const userDocSnap = await getDoc(userDocRef);
-  
+
           const user = userDocSnap.data();
-  
+
           return { ...item, user };
         });
-  
+
         const chatData = await Promise.all(promises);
-  
+
         setChats(chatData.sort((a, b) => b.updateAt - a.updateAt));
       } else {
         console.log("No se encontraron chats para el usuario actual");
         setChats([]);
       }
     });
-  
+
     return () => {
       unSub();
     };
   }, [currentUser.id]);
 
   const handleSelect = async (chat) => {
-    changeChat(chat.chatId, chat.user);
-  }
+    const userChats = chats.map(item => {
+      const {user, ...rest} = item;
+      return rest;
+    });
+
+    const chatIndex = userChats.findIndex(item => item.chatId === chat.chatId)
+
+    userChats[chatIndex].isSeen = true;
+
+    const userChatsRef = doc(db, "userchats", currentUser.id);
+
+    try {
+      await updateDoc(userChatsRef, {
+        chats: userChats,
+      });
+      changeChat(chat.chatId, chat.user);
+    } catch(err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="chatList">
@@ -60,14 +78,14 @@ const ChatList = () => {
         )}
       </div>
       {chats.map(chat => (
-        <div className="item" key={chat.chatId} onClick={() => handleSelect(chat)}>
+        <div className="item" key={chat.chatId} onClick={() => handleSelect(chat)} style={{backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",}}>
           <img src={chat.user.avatar} alt="Avatar" />
           <div className="texts">
             <span>{chat.user.username}</span>
             <p>{chat.lastMessage}</p>
           </div>
         </div>
-      ))};
+      ))}
       {addMode && <AddUser />}
     </div>
   );
